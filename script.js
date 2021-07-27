77025bb0c1363730027a00c3bddf4c10dc63e038
Denis Ilukovich <45496175+dediluk@users.noreply.github.com>
2021-07-27 15:19:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let video_sections = document.querySelectorAll('.style-scope .ytd-playlist-video-list-renderer'),
     index = 5;
 function deleting() {
-    popup_menu_button = video_sections[index].querySelector('#menu #button');
+    let popup_menu_button = video_sections[index].querySelector('#menu #button');
     if (!popup_menu_button) {
-        console.log('thats all, folks');
+        console.log("======== That's all, folks ========");
         clearInterval(interval);
     }
     popup_menu_button.click();
